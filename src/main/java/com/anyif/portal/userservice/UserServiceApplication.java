4126f4825b6f2087e3daa2db7bcac04828598d33
JesContes <568494724@qq.com>
2024-01-03 14:53:40 +0800

    initial project

---
+++ src/main/java/com/anyif/portal/userservice/UserServiceApplication.java
@@ -0,0 +1,18 @@
+package com.anyif.portal.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JesContes
+ * @description service entry
+ * @since 2024/1/3
+ */
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
